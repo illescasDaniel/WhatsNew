d8356aabfcf604b014dfa5837c7c4b6e0476ef51
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2019-03-30 13:29:17 +0100

    Initial Commit

---
+++ WhatsNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsNew
+//
+//  Created by Daniel Illescas Romero on 30/03/2019.
+//  Copyright © 2019 Daniel Illescas Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
